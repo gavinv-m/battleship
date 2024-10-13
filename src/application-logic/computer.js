24cb09d046b9de9801881590043701fa96e916f5
Gavin <gavinvm@protonmail.com>
2024-10-13 13:56:10 +0200

    refactor: Separate computer logic to own module

---
+++ src/application-logic/computer.js
@@ -0,0 +1,131 @@
+export default class ComputerPlayer {
+  constructor() {
+    this.attackCoords = [];
+    this.hits = 0;
+    this.precision = false;
+    this.preciseCoords = [];
+    this.firstHit = [];
+    this.removed = false;
+  }
+
+  shuffle() {
+    for (let i = this.attackCoords.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.attackCoords[i], this.attackCoords[j]] = [
+        this.attackCoords[j],
+        this.attackCoords[i],
+      ];
+    }
+  }
+
+  getAttackCoordinates() {
+    if (this.preciseCoords.length > 0) {
+      const attackCoords = this.preciseCoords.pop();
+      const index = this.attackCoords.findIndex(
+        (coord) => coord[0] === attackCoords[0] && coord[1] === attackCoords[1],
+      );
+      this.attackCoords.splice(index, 1);
+      return attackCoords;
+    } else {
+      this.precision = false;
+      this.removed = false;
+      this.shuffle();
+      return this.attackCoords.pop();
+    }
+  }
+
+  addNewPreciseCoords(isVertical, r, c, row, col) {
+    const newCoords = [];
+    if (isVertical) {
+      if (r - 1 >= 0) newCoords.push([r - 1, col]);
+      if (r + 1 < 10) newCoords.push([r + 1, col]);
+    } else {
+      if (c - 1 >= 0) newCoords.push([row, c - 1]);
+      if (c + 1 < 10) newCoords.push([row, c + 1]);
+    }
+
+    newCoords.forEach((coord) => {
+      const isInAttackCoords = this.attackCoords.some(
+        ([x, y]) => x === coord[0] && y === coord[1],
+      );
+      const isInPreciseCoords = this.preciseCoords.some(
+        ([x, y]) => x === coord[0] && y === coord[1],
+      );
+      if (isInAttackCoords && !isInPreciseCoords) {
+        this.preciseCoords.push(coord);
+      }
+    });
+  }
+
+  refineAttackStrategy(attackCoords) {
+    const [row, col] = this.firstHit;
+    const [r, c] = attackCoords;
+
+    const isHorizontal = row === r;
+    const isVertical = col === c;
+
+    this.preciseCoords = this.preciseCoords.filter(([x, y]) => {
+      return (isHorizontal && x === row) || (isVertical && y === col);
+    });
+
+    this.addNewPreciseCoords(isVertical, r, c, row, col);
+
+    this.removed = true;
+    this.hits += 1;
+  }
+
+  addCoordIfNotAttacked(target) {
+    const notAttacked = this.attackCoords.some(
+      (coord) => coord[0] === target[0] && coord[1] === target[1],
+    );
+    if (notAttacked) this.preciseCoords.push(target);
+  }
+
+  predictCoordinates(recentCoordinates) {
+    const [row, col] = recentCoordinates;
+
+    // Horizontal coordinates
+    for (let i = col - 1; i < col + 2; i += 2) {
+      if (i >= 0 && i <= 9) {
+        this.addCoordIfNotAttacked([row, i]);
+      }
+    }
+
+    // Vertical coordinates
+    for (let i = row - 1; i < row + 2; i += 2) {
+      if (i >= 0 && i <= 9) {
+        this.addCoordIfNotAttacked([i, col]);
+      }
+    }
+  }
+
+  initiatePrecisionAttack(attackCoords) {
+    this.hits += 1;
+    this.firstHit = attackCoords;
+    this.precision = true;
+    this.predictCoordinates(attackCoords);
+  }
+
+  processAttackResult(hitTarget, attackCoords) {
+    /**
+     * Check if precision is, and another part was hit
+     * If another part hit determine direction to continue attack
+     */
+    if (hitTarget && this.precision && !this.removed) {
+      this.refineAttackStrategy(attackCoords);
+    }
+
+    // If first hit on a ship enable precision
+    if (hitTarget && !this.precision) {
+      this.initiatePrecisionAttack(attackCoords);
+    }
+  }
+
+  generatePossibleAttacks(gameboard) {
+    gameboard.forEach((row, rowIndex) => {
+      row.forEach((cell, colIndex) => {
+        this.attackCoords.push([rowIndex, colIndex]);
+      });
+    });
+  }
+}
--- src/application-logic/game.js
+++ src/application-logic/game.js
@@ -1,18 +1,13 @@
 import Player from './player';
 import Ship from './ship';
 import Coordinates from './coordinates';
+import ComputerPlayer from './computer';
 
 export default class Game {
   constructor() {
     this.player1 = null;
     this.player2 = null;
-
-    this.computerAttackCoords = [];
-    this.computerHits = 0;
-    this.precision = false;
-    this.preciseCoords = [];
-    this.firstHit = [];
-    this.removed = false;
+    this.computerPlayer = new ComputerPlayer();
 
     this.gameWon = false;
     this.gameActive = false;
@@ -23,7 +18,7 @@ export default class Game {
   startGame() {
     this.player1 = new Player();
     this.player2 = new Player();
-    this.generatePossibleAttacks();
+    this.computerPlayer.generatePossibleAttacks(this.player1.gameboard.board);
   }
 
   placeShips() {
@@ -48,20 +43,7 @@ export default class Game {
   }
 
   playComputerTurn() {
-    let attackCoords;
-
-    if (this.preciseCoords.length > 0) {
-      attackCoords = this.preciseCoords.pop();
-      const index = this.computerAttackCoords.findIndex(
-        (coord) => coord[0] === attackCoords[0] && coord[1] === attackCoords[1],
-      );
-      this.computerAttackCoords.splice(index, 1);
-    } else {
-      this.precision = false;
-      this.removed = false;
-      this.shuffle();
-      attackCoords = this.computerAttackCoords.pop(); // Returns to us the last element
-    }
+    const attackCoords = this.computerPlayer.getAttackCoordinates();
 
     const computerWon = this.player1.gameboard.receiveAttack(
       attackCoords,
@@ -70,53 +52,7 @@ export default class Game {
 
     // Query for number of hits on player board
     const hitTarget = this.checkHits();
-
-    // Hit target using precision, determine orientation, remove misaligned coords
-    if (hitTarget && this.precision && !this.removed) {
-      // First coordinates are the ones that enabled precision
-      const [row, col] = this.firstHit;
-      const [r, c] = attackCoords;
-
-      const isHorizontal = row === r;
-      const isVertical = col === c;
-
-      this.preciseCoords = this.preciseCoords.filter(([x, y]) => {
-        return (isHorizontal && x === row) || (isVertical && y === col);
-      });
-
-      const newCoords = [];
-      if (isVertical === true) {
-        if (r - 1 >= 0) newCoords.push([r - 1, col]);
-        if (r + 1 < 10) newCoords.push([r + 1, col]);
-      } else {
-        if (c - 1 >= 0) newCoords.push([row, c - 1]);
-        if (c + 1 < 10) newCoords.push([row, c + 1]);
-      }
-
-      // Check if the coordinate not attacked and not already in precise coords
-      newCoords.forEach((coord) => {
-        const isInComputerAttackCoords = this.computerAttackCoords.some(
-          ([x, y]) => x === coord[0] && y === coord[1],
-        );
-        const isInPreciseCoords = this.preciseCoords.some(
-          ([x, y]) => x === coord[0] && y === coord[1],
-        );
-        if (isInComputerAttackCoords && !isInPreciseCoords) {
-          this.preciseCoords.push(coord);
-        }
-      });
-
-      this.removed = true;
-      this.computerHits += 1;
-    }
-
-    // First hit
-    if (hitTarget === true && this.precision === false) {
-      this.computerHits += 1;
-      this.firstHit = attackCoords;
-      this.precision = true;
-      this.predictCoordinates(attackCoords);
-    }
+    this.computerPlayer.processAttackResult(hitTarget, attackCoords);
 
     // Dispatch to update ui
     this.dispatchAttackEvent('computer', attackCoords, this.player1.gameboard);
@@ -138,51 +74,7 @@ export default class Game {
       0,
     );
 
-    return occupiedHits !== this.computerHits;
-  }
-
-  predictCoordinates(recentCoordinates) {
-    const [row, col] = recentCoordinates;
-
-    // Horizontal coordinates
-    for (let i = col - 1; i < col + 2; i += 2) {
-      if (i >= 0 && i <= 9) {
-        const target = [row, i];
-        const notAttacked = this.computerAttackCoords.some(
-          (coord) => coord[0] === target[0] && coord[1] === target[1],
-        );
-        if (notAttacked === true) this.preciseCoords.push(target);
-      }
-    }
-
-    // Vertical coordinates
-    for (let i = row - 1; i < row + 2; i += 2) {
-      if (i >= 0 && i <= 9) {
-        const target = [i, col];
-        const notAttacked = this.computerAttackCoords.some(
-          (coord) => coord[0] === target[0] && coord[1] === target[1],
-        );
-        if (notAttacked === true) this.preciseCoords.push(target);
-      }
-    }
-  }
-
-  generatePossibleAttacks() {
-    this.player1.gameboard.board.forEach((row, rowIndex) => {
-      row.forEach((cell, colIndex) => {
-        this.computerAttackCoords.push([rowIndex, colIndex]);
-      });
-    });
-  }
-
-  shuffle() {
-    for (let i = this.computerAttackCoords.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [this.computerAttackCoords[i], this.computerAttackCoords[j]] = [
-        this.computerAttackCoords[j],
-        this.computerAttackCoords[i],
-      ];
-    }
+    return occupiedHits !== this.computerPlayer.hits;
   }
 
   getPlayers() {
